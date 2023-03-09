7cf81efa4103c4fdea8ae9e47e904d1450bf1399
macservis <fboqiyev1065@gmail.com>
2023-03-09 17:52:40 +0500

    stage-0 module-3 task-6 updated

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -3,7 +3,8 @@ package lang.print.gaps.task6;
 public class IntegerTypeCasting {
     public static void main(String[] args) {
         long first = 1000;
-//        var second = (var) first;
-        int seconds = (int) first;
+        var second = (int) first;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
